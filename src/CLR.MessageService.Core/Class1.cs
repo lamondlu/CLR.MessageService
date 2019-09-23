321811e626e1c7776606337ea1a5cf597a87c321
Lamond <309728709@qq.com>
2019-09-23 01:34:17 -0400

    add docker support

--- src/CLR.MessageService.Core/Class1.cs
+++ src/CLR.MessageService.Core/Class1.cs
@@ -4,5 +4,9 @@ namespace CLR.MessageService.Core
 {
     public class Class1
     {
+        public static void ABC()
+        {
+            Console.WriteLine($"heLLO wORLD{"123".ToLower()}");
+        }
     }
 }
