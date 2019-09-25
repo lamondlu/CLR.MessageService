3c414afe9741b222878ca754c29e98497c0d41d4
Lamond Lu <lunan@shintechchina.com>
2019-09-25 23:01:57 +0800

    add package

---
+++ src/CLR.MessageService.Core/Models/Project.cs
@@ -0,0 +1,11 @@
+namespace CLR.MessageService.Core.Models
+{
+    public class Project
+    {
+        public string ProjectId { get; }
+
+        public string ProjectName { get; set; }
+
+        
+    }
+}
