b61a35650c91b35ee1266f87257028903ee5d489
Lamond <309728709@qq.com>
2019-10-09 03:46:28 -0400

    update code

--- src/CLR.MessageService.WebApi/Startup.cs
+++ src/CLR.MessageService.WebApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using App.Metrics;
 
 namespace CLR.MessageService.WebApi
 {
@@ -26,6 +27,45 @@ namespace CLR.MessageService.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+              string IsOpen = Configuration.GetSection("InfluxDB")["IsOpen"].ToLower();
+            if (IsOpen == "true")
+            {
+                string database = Configuration.GetSection("InfluxDB")["DataBaseName"];
+                string InfluxDBConStr = Configuration.GetSection("InfluxDB")["ConnectionString"];
+                string app = Configuration.GetSection("InfluxDB")["app"];
+                string env = Configuration.GetSection("InfluxDB")["env"];
+                string username = Configuration.GetSection("InfluxDB")["username"];
+                string password = Configuration.GetSection("InfluxDB")["password"];
+ 
+                var uri = new Uri(InfluxDBConStr);
+ 
+                var metrics = AppMetrics.CreateDefaultBuilder()
+                .Configuration.Configure(
+                options =>
+                {
+                    options.AddAppTag(app);
+                    options.AddEnvTag(env);
+                })
+                .Report.ToInfluxDb(
+                options =>
+                {
+                    options.InfluxDb.BaseUri = uri;
+                    options.InfluxDb.Database = database;
+                    options.InfluxDb.UserName = username;
+                    options.InfluxDb.Password = password;
+                    options.HttpPolicy.BackoffPeriod = TimeSpan.FromSeconds(30);
+                    options.HttpPolicy.FailuresBeforeBackoff = 5;
+                    options.HttpPolicy.Timeout = TimeSpan.FromSeconds(10);
+                    options.FlushInterval = TimeSpan.FromSeconds(5);
+                })
+                .Build();
+ 
+                services.AddMetrics(metrics);
+                services.AddMetricsReportScheduler();
+                services.AddMetricsTrackingMiddleware();
+                services.AddMetricsEndpoints();
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +81,27 @@ namespace CLR.MessageService.WebApi
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            #region 注入Metrics
+            string IsOpen = Configuration.GetSection("InfluxDB")["IsOpen"].ToLower();
+            if (IsOpen == "true")
+            {
+                app.UseMetricsAllMiddleware();
+                // Or to cherry-pick the tracking of interest
+                app.UseMetricsActiveRequestMiddleware();
+                app.UseMetricsErrorTrackingMiddleware();
+                app.UseMetricsPostAndPutSizeTrackingMiddleware();
+                app.UseMetricsRequestTrackingMiddleware();
+                app.UseMetricsOAuth2TrackingMiddleware();
+                app.UseMetricsApdexTrackingMiddleware();
+ 
+                app.UseMetricsAllEndpoints();
+                // Or to cherry-pick endpoint of interest
+                app.UseMetricsEndpoint();
+                app.UseMetricsTextEndpoint();
+                app.UseEnvInfoEndpoint();
+            }
+            #endregion
+
             app.UseMvc();
         }
     }
